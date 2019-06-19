ca9aa68684a15f5d1b2d55248b24279e7c5563e8
github username <github email address>
2019-06-19 12:08:55 -0400

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react'
 import TransactionsList from './TransactionsList'
 import Search from './Search'
-import {transactions} from '../transactionsData'
+
 
 class AccountContainer extends Component {
 
-  constructor() {
-    super()
 
-    // get a default state working with the data imported from TransactionsData
-    // use this to get the functionality working
-    // then replace the default transactions with a call to the API
+    state = {
+      transactions: [],
+      userInput: '',
+    }
 
+
+
+  componentDidMount = () => {
+    fetch("https://boiling-brook-94902.herokuapp.com/transactions")
+      .then(response => response.json())
+      .then(transactions => this.setState({
+        transactions: transactions
+      }))
   }
 
-  handleChange(event) {
-    // your code here
+
+  handleChange(userInput) {
+    this.setState({
+      userInput: userInput
+    })
   }
+
+  filter = () => {
+    return this.state.transactions.filter(transaction => {
+      debugger
+      return transaction.category.toLowerCase().includes(this.state.userInput.toLowerCase())
+    })
+  }
+
+
 
   render() {
-
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange}/>
+        <TransactionsList transactions={this.filter()}/>
       </div>
     )
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Search = () => {
+class Search extends Component {
+
+  state = {
+    userInput: '',
+  }
+
+  handleInput= (event) => {
+    this.setState ({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleChange = () => {
+    console.log("hehehehehshetnes", this.state.userInput)
+    this.props.handleChange(this.state.userInput)
+  }
+
+  render() {
   return (
     <div className="ui huge fluid icon input">
       <input
         type="text"
         placeholder={"Search your Recent Transactions"}
+        onChange={this.handleChange}
+        name="userInput"
       />
       <i className="circular search link icon"></i>
     </div>
   )
+  }
 }
+
 
 export default Search
