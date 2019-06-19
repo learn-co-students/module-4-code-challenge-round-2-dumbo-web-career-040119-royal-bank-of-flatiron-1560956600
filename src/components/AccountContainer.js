22088988618b3437d7c69d077a1b8ec05283c39e
merjench <merjen.chary@gmail.com>
2019-06-19 12:12:54 -0400

    Search almost done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,27 +3,55 @@ import TransactionsList from './TransactionsList'
 import Search from './Search'
 import {transactions} from '../transactionsData'
 
+
 class AccountContainer extends Component {
 
-  constructor() {
-    super()
+  state = {
+    transactions: [],
+    searchFilter: []
+  }
 
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
 
-  }
+    componentDidMount() {
+      fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data)
+          this.setState({
+            transactions: data
+          })
+      })
+    }
 
-  handleChange(event) {
-    // your code here
-  }
+  // handleChange = (event, { value }) => {
+  //   // console.log(event.target)
+  //   this.setState({
+  //     searchFilter: value
+  //   }, () => console.log(this.state.searchFilter))
+  // }
+
+  handleChange = (event, { value }) => {
+     this.setState({ searchFilter: value })
+   }
+
+   filterChange = () => {
+     let array = this.state.transactions
+     let filteredlist = array.filter(list => list.description.toLowerCase().includes(this.state.searchFilter))
+     return filteredlist
+   }
 
   render() {
 
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search
+         onSearchChange={this.handleChange}
+         showNoResults={false}/>
+        <TransactionsList
+         transactions={this.filterChange()}/>
       </div>
     )
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Search = () => {
+const Search = (props) => {
   return (
     <div className="ui huge fluid icon input">
       <input
