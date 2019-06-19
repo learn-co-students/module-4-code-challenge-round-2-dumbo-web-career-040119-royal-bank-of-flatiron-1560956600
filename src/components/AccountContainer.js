7f00fe6b577b5014a283dd50d3108ab90d4be5bc
ftse105 <ftse105@gmail.com>
2019-06-19 11:55:16 -0400

    lit

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,26 +4,30 @@ import Search from './Search'
 import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
-
-  constructor() {
-    super()
-
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
-
+  state = {
+    searchTerm: ""
   }
 
-  handleChange(event) {
-    // your code here
+  handleChange = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    })
   }
+
 
   render() {
+    const filteredTransactions = transactions.filter(transaction => {
+      return (transaction.description.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+      || transaction.category.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
+    })
 
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} searchTerm={this.state.searchTerm}/>
+        <TransactionsList transactions={filteredTransactions}/>
       </div>
     )
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const Search = () => {
+const Search = (props) => {
+  console.log(props.searchTerm);
   return (
     <div className="ui huge fluid icon input">
       <input
+        value={props.searchTerm}
+        onChange={props.handleChange}
         type="text"
         placeholder={"Search your Recent Transactions"}
       />
